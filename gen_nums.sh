78cfe0b64d422e57c5b1e71147804ba1ff9f563d
Nia M <unikorn.nkm31@gmail.com>
2026-02-24 18:12:18 -0500

    feat: Update script that generates random numbers

--- gen_nums.sh
+++ gen_nums.sh
@@ -1,4 +1,15 @@
-for i in {1..20}; do
+count=20
+
+while getopts "n:" opt; do
+    case $opt in 
+        n) count=$OPTARG ;;
+        *) echo "Usage: $0 [-n number]" >&2
+            exit 1 ;;
+    esac
+done
+
+
+for ((i=1; i<=count;i++)); do
     num=$(( $RANDOM % 6 + 1))
     echo "$num"
 
